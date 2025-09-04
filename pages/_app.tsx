9a6ceced04cb79efbac2c38eeb60980f3d49ff7a
Adrien Marques <43345482+marcucus@users.noreply.github.com>
2025-09-04 22:59:24 +0200

    add analytics vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import Head from 'next/head'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
     <SpeedInsights />
+    <Analytics />
       <Head>
         <link href="/css/styles.css" rel="stylesheet" />
       </Head>
